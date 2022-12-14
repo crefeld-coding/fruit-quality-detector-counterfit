dd580f41b2cbf976670ad150a774e92cdb7ee1e0
Filip Furmanek <ffurmanek@crefeld.org>
2022-12-14 14:03:38 -0500

    Add Azure Custom Vision code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from counterfit_connection import CounterFitConnection
 import io
 from counterfit_shims_picamera import PiCamera
+from msrest.authentication import ApiKeyCredentials
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 print("Import done.")
 
 CounterFitConnection.init('127.0.0.1', 5000)
@@ -19,4 +21,24 @@ print("Capture done.")
 with open('image.jpg', 'wb') as image_file:
     image_file.write(image.read())
 
-print("Write done. AKA DONE!")
+print("Write done.")
+
+prediction_url = 'REPLACE_ME'
+prediction_key = 'REPLACE_ME'
+
+parts = prediction_url.split('/')
+endpoint = 'https://' + parts[2]
+project_id = parts[6]
+iteration_name = parts[9]
+
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(endpoint, prediction_credentials)
+print("Predictor created.")
+
+image.seek(0)
+results = predictor.classify_image(project_id, iteration_name, image)
+print("Image submitted, results received.")
+
+for prediction in results.predictions:
+    print(f'{prediction.tag_name}:\t{prediction.probability * 100:.2f}%')
+print("Results done.")
